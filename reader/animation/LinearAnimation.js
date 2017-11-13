9d0553289150086c2db84d3b71c6e6ecdf07f5d8
mateuspedroza <mpcmarques@icloud.com>
2017-11-13 11:23:15 +0000

    Linear animation fix

--- reader/animation/LinearAnimation.js
+++ reader/animation/LinearAnimation.js
@@ -1,10 +1,13 @@
-function LinearAnimation (scene, controlPoints, speed) {
+function LinearAnimation(scene, controlPoints, speed) {
     Animation.call(this, scene);
 
     this.controlPoints = controlPoints;
     this.speed = speed;
 
-    this.point = controlPoints[0];
+    this.point = [];
+    this.point[0] = controlPoints[0][0];
+    this.point[1] = controlPoints[0][1];
+    this.point[2] = controlPoints[0][2];
     this.currentControlPoint = 0;
     this.ended = false;
 }
@@ -12,43 +15,37 @@ function LinearAnimation (scene, controlPoints, speed) {
 LinearAnimation.prototype = Object.create(Animation);
 LinearAnimation.prototype.constructor = LinearAnimation;
 
-LinearAnimation.prototype.animate = function(currTime){
+LinearAnimation.prototype.animate = function (currTime) {
 
-    if(this.ended)
+    if (this.ended)
         return;
 
+    if (this.initialTime == null)
+        this.initialTime = currTime;
+
     // delta T
-    if(this.currentTime != null) {
-        var deltaT = ((currTime - this.currentTime) / 1000);
+    var deltaT = ((currTime - this.initialTime) / 1000);
 
-        // X = Xo + v * t.
-        this.point[0] = this.controlPoints[this.currentControlPoint][0] + deltaT * this.speed;
-        this.point[2] = this.controlPoints[this.currentControlPoint][2] + deltaT * this.speed;
+    // TODO -> MOVIMENTO PARA TRAS
 
-        if (this.point[0] >= this.controlPoints[this.currentControlPoint + 1][0]) {
-            this.point[0] = this.controlPoints[this.currentControlPoint + 1][0];
-        }
+    // X = Xo + v * t.
+    this.point[0] = this.controlPoints[this.currentControlPoint][0] + deltaT * this.speed;
+    this.point[2] = this.controlPoints[this.currentControlPoint][2] + deltaT * this.speed;
 
-        if (this.point[2] >= this.controlPoints[this.currentControlPoint + 1][2]) {
-            this.point[2] = this.controlPoints[this.currentControlPoint + 1][2];
-        }
-
-        if (this.point[2] >= this.controlPoints[this.currentControlPoint + 1][2] &&
-            this.point[0] >= this.controlPoints[this.currentControlPoint + 1][2]) {
-            this.currentControlPoint += 1;
-        }
+    if (this.point[2] >= this.controlPoints[this.currentControlPoint + 1][2] &&
+        this.point[0] >= this.controlPoints[this.currentControlPoint + 1][2]) {
+        this.currentControlPoint += 1;
+        this.initialTime = currTime;
     }
 
     if (this.currentControlPoint == this.controlPoints.length - 1)
         this.ended = true;
-
-    this.currentTime = currTime;
 };
 
-LinearAnimation.prototype.update = function(currTime) {
-   this.animate(currTime);
+LinearAnimation.prototype.update = function (currTime) {
+    this.animate(currTime);
 };
 
-LinearAnimation.prototype.display = function() {
+LinearAnimation.prototype.display = function () {
     this.scene.translate(this.point[0], 0, this.point[2]);
 };
