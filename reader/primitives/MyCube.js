c35cacaedc960ff04ffaa53ba7c5001fff2b3a3b
mateuspedroza <mpcmarques@icloud.com>
2017-12-11 11:37:59 +0000

    Added board wood appearance.

--- reader/primitives/MyCube.js
+++ reader/primitives/MyCube.js
@@ -4,6 +4,7 @@ function MyCube(scene){
     this.rect = new MyRectangle(scene, 0, 1, 1, 0);
 
     this.topAppearance = null;
+    this.appearance = null;
 }
 
 MyCube.prototype = Object.create(MyCube);
@@ -13,8 +14,15 @@ MyCube.prototype.setTopAppearance = function(appearance) {
     this.topAppearance = appearance;
 };
 
+MyCube.prototype.setAppearance = function(apperance){
+    this.appearance = apperance;
+};
+
 MyCube.prototype.display = function () {
     this.scene.pushMatrix();
+
+    if(this.appearance != null)
+        this.appearance.apply();
 
     this.scene.pushMatrix();
     this.scene.translate(1, 0, 0);
@@ -38,6 +46,12 @@ MyCube.prototype.display = function () {
     this.rect.display();
     this.scene.popMatrix();
 
+    // down
+    this.scene.pushMatrix();
+    this.scene.rotate(Math.PI / 2, 1, 0, 0);
+    this.rect.display();
+    this.scene.popMatrix();
+
     // up
     this.scene.pushMatrix();
     this.scene.translate(0,1,1);
@@ -48,11 +62,9 @@ MyCube.prototype.display = function () {
     this.rect.display();
     this.scene.popMatrix();
 
-    // down
-    this.scene.pushMatrix();
-    this.scene.rotate(Math.PI / 2, 1, 0, 0);
-    this.rect.display();
     this.scene.popMatrix();
+};
 
-    this.scene.popMatrix();
+MyCube.prototype.scaleTexCoords = function(ampS, ampT){
+    this.rect.scaleTexCoords(ampS, ampT);
 };
