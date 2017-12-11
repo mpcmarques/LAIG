d7ae90f1cb1aa09d663179de801a0a4ad77fde00
diogomotapinto <diogomotapinto@gmail.com>
2017-12-11 10:41:08 +0000

    Added Pieces

--- reader/primitives/MyPiece.js
+++ reader/primitives/MyPiece.js
@@ -1,16 +1,8 @@
 function MyPiece(scene){
     MyPrimitive.call(this, scene);
-//  this.cyl = new   MyCylinderWithCover(this.scene, 1, 1, 1, 20,20,1,1);
+
 }
 
 MyPiece.prototype = Object.create(MyPrimitive.prototype);
-MyPiece.prototype.constructor = MyPeca;
+MyPiece.prototype.constructor = MyPiece;
 
-/*
-MyPiece.prototype.display = function(){
-
-  this.scene.pushMatrix();
-  this.cyl.display();
-  this.scene.popMatrix();
-
-}*/
